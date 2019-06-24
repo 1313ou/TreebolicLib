132ca39df46e003b4301f4c962efeffbfca67a3a
bbou <bbou@ac-toulouse.fr>
2019-06-24 08:33:14 +0200

    lint

--- treebolicGlue/src/main/java/treebolic/glue/Image.java
+++ treebolicGlue/src/main/java/treebolic/glue/Image.java
@@ -142,7 +142,7 @@ public class Image implements treebolic.glue.iface.Image, Serializable
 	 *
 	 * @param imageByteArray byte array
 	 */
-	@SuppressWarnings("WeakerAccess")
+	@SuppressWarnings({"WeakerAccess", "deprecation"})
 	public void setFromByteArray(@NonNull final byte[] imageByteArray)
 	{
 		final Options opt = new Options();
