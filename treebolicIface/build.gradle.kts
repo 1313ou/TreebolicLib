f2eac77663ddf7125d7ef95c4359c807ee3fc3b7
1313ou <1313ou@gmail.com>
2025-06-15 11:18:06 +0200

    migration to kts

---
+++ treebolicIface/build.gradle.kts
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019. Bernard Bou <1313ou@gmail.com>
+ */
+
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+}
+
+private val vCompileSdk by lazy { rootProject.extra["compileSdk"] as Int }
+private val vMinSdk by lazy { rootProject.extra["minSdk"] as Int }
+
+android {
+
+    namespace = "org.treebolic.iface"
+
+    compileSdk = vCompileSdk
+
+    defaultConfig {
+        minSdk = vMinSdk
+        multiDexEnabled = true
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(libs.annotation)
+    implementation(libs.core.ktx)
+
+    coreLibraryDesugaring(libs.desugar)
+}
