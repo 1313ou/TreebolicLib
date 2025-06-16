9951c36cd114d0884a70dea0060dc0e30f0cf159
1313ou <1313ou@gmail.com>
2025-06-16 18:47:53 +0200

    to kts

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022. Bernard Bou <1313ou@gmail.com>
+ */
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenLocal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenLocal()
+        mavenCentral()
+    }
+}
+
+include(":treebolicGlue")
+include(":treebolicIface")
